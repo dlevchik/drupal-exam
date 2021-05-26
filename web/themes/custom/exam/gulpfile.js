72735fb3d419ee64f1fbc1df503f7e417ab69562
dlevchik <kanalqwesta@gmail.com>
2021-05-26 17:24:01 +0300

    container and colours setup

--- web/themes/custom/exam/gulpfile.js
+++ web/themes/custom/exam/gulpfile.js
@@ -58,7 +58,7 @@ function js () {
 // Static Server + watching scss/html files
 function serve () {
   browserSync.init({
-    proxy: 'http://yourdomain.com',
+    proxy: '',
   })
 
   gulp.watch([paths.scss.watch, paths.scss.bootstrap], styles).on('change', browserSync.reload)
